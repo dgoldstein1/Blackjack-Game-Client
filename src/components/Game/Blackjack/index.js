ca3971f95459d749867325b24a9b4eb2e8ed252b
kerenKi <kkinberg13@gmail.com>
2019-06-07 12:40:54 +0200

    Displaying other player's cards

--- src/components/Game/Blackjack/index.js
+++ src/components/Game/Blackjack/index.js
@@ -17,15 +17,15 @@ class Blackjack extends Component {
   state = {
     disabledButton: false
   }
-
+  
   componentDidMount() {
     this.props.getDeck();
   }
-
+  
   componentDidUpdate() {
-   this.props.getResults();
+    this.props.getResults();
   }
-
+  
   disableButtons = () => {
     this.setState({
       disabledButton: true
@@ -41,91 +41,113 @@ class Blackjack extends Component {
     this.props.updatePlayer('draw');
     this.props.turnPlayed();
   };
-
+  
   // Call game
   call = () => {
     this.disableButtons()
     this.props.updatePlayer('call');
-
+    
   };
-
+  
   onEndGame = () => {
     this.props.gameEnded()
   }  
-
-
+  
   render() {
     // Get deck
     const deck = this.props.deck;
-
+    
     // Get draws
     const draws = this.props.draws;
-
+    
     // Get score
     const score = this.props.score;
-
+    
     //Get user name
     const userName = this.props.userLogedIn.name
-
+    
     //Get current player name
     const currentPlayer = this.props.currentTurn.name
-
+    
+    //Get other player object
+    const other = this.props.playersInGame.find(player => player.id !== this.props.userLogedIn.id)
+    console.log('other:', other)
     // Display draws
     const displayDraws =
-      draws &&
-      [...draws].map(card => <VisibleCard key={card.code} card={card} />);
+    draws &&
+    [...draws].map(card => <VisibleCard key={card.code} card={card} />);
+    
+    // Display other draws
+    const displayOtherDraws =
+    other.draws[0] &&
+    [...other.draws].map(card => <VisibleCard key={card.code} card={card} />);
 
     return (
       <div>
-        <h1>{userName}'s cards</h1>
-
-        <button onClick={this.onEndGame}>End game</button>
-
-        <div>
-          {!deck && <p>Hold on, shuffeling cards..</p>}
-          {deck && draws.length >= 0 && (
-            <span>
-              Current turn: {currentPlayer}
-              <br /> <br />
-              <b>Card amount</b>: {score}
-            </span>
-          )}
-        </div>
-
-        {deck && draws.length === 0 && (
-          <div>
-            <FaceDownCard card={deck[0]} />
-            <FaceDownCard card={deck[0]} />
-          </div>
-        )}
-
-        {deck && draws.length === 1 && (
-          <div>
-            <VisibleCard card={draws[0]} />
-            <FaceDownCard card={deck[0]} />
-          </div>
-        )}
-
-        <div>{deck && draws.length > 1 && displayDraws}</div>
-
-
-
-        {deck && this.props.userLogedIn.id === this.props.currentTurn.id && (
-          <div>
-            <button onClick={this.draw} disabled={this.state.disabledButton}>Draw</button>
-            <button onClick={this.call} disabled={this.state.disabledButton}>Call</button>
-          </div>
-        )}
-        {deck && this.props.userLogedIn.id !== this.props.currentTurn.id && (
-          <div>
-            <p>Waiting for {currentPlayer}'s move..  </p>
-          </div>
-        )}
+      <button onClick={this.onEndGame}>End game</button>
+      
+      <h1>{userName}'s cards</h1>
+      <div>
+      {!deck && <p>Hold on, shuffeling cards..</p>}
+      {deck && draws.length >= 0 && <span>
+        <b>Card amount</b>: {score} </span>}
       </div>
-    );
-  }
-}
-
+        
+      {deck && draws.length === 0 && (
+        <div>
+        <FaceDownCard card={deck[0]} />
+        <FaceDownCard card={deck[0]} />
+        </div>
+        )}
+        
+      {deck && draws.length === 1 && (
+        <div>
+        <VisibleCard card={draws[0]} />
+        <FaceDownCard card={deck[0]} />
+        </div>
+        )}
+          
+      <div>{deck && draws.length > 1 && displayDraws}</div>
+      
+      {deck && draws.length >= 0 && <p> Current turn: {currentPlayer}</p> }
+      
+      {deck && this.props.userLogedIn.id === this.props.currentTurn.id && (<div>
+        <button onClick={this.draw} disabled={this.state.disabledButton}>Draw</button>
+        <button onClick={this.call} disabled={this.state.disabledButton}>Call</button>
+        </div>)}
+        
+      {deck && this.props.userLogedIn.id !== this.props.currentTurn.id && (
+        <div>
+        <p>Waiting for {currentPlayer}'s move..  </p>
+        </div>
+        )}
+          
+      {other && (<div>
+        <h2>{other.name}'s cards</h2>
+        <p><b>Card amount</b>: {other.score}</p>
+     
+        {deck && other.draws.length === 0 && (
+          <div>
+          <FaceDownCard card={deck[0]} />
+          <FaceDownCard card={deck[0]} />
+          </div>
+          )}
+          
+          { other.draws.length === 1 && (
+            <div>
+            <VisibleCard card={other.draws[0]} />
+            <FaceDownCard card={other.draws[0]} />
+            </div>
+            )}
+            <div>{ other.draws.length > 1 && displayOtherDraws}</div>}
+       
+            </div>)}
+            
+            </div>
+            );
+          }
+        }
+                  
 const mapStateToProps = state => {
   return {
     deck: state.deck,
@@ -141,4 +163,5 @@ export default connect(
   mapStateToProps,
   { getDeck, drawCard, updatePlayer, turnPlayed,
     getResults,gameEnded}
-)(Blackjack);
+    )(Blackjack);
+                      
